9810cd4e58b07d42fb983ce2588bc7e0b08df36c
Giuseppe Menti <mentifg@gmail.com>
2018-04-18 14:28:26 -0300

    Fix method to print error

--- index.js
+++ index.js
@@ -4,35 +4,56 @@ const fs = require('fs')
 const path = require('path')
 
 const FILE_PATH = path.join(__dirname, 'data.json')
+if (!fs.existsSync(FILE_PATH)) {
+  fs.writeFileSync(FILE_PATH, JSON.stringify([]))
+}
 
 const app = new Koa()
 app.use(require('@koa/cors')())
 app.use(require('koa-bodyparser')())
 
 app.use(
-  new Router()
-    .post('/', (ctx, next) => {
-      fs.readFile(FILE_PATH, data => {
-        const dataList = JSON.parse(data) || []
-        dataList.push({
-          info: ctx.request.body,
-          date: new Date()
-        })
-        fs.writeFile(FILE_PATH, dataList)
-        ctx.status = 204
-        ctx.body = null
-        next()
+  (new Router())
+    .post('/', ctx => new Promise(resolve => {
+      fs.readFile(FILE_PATH, (err, data) => {
+        var dataList = []
+        if (!err) {
+          try {
+            dataList = JSON.parse(data)
+          } catch (err) {
+            dataList = []
+          }
+          dataList.unshift({
+            info: ctx.request.body,
+            date: new Date()
+          })
+          ctx.status = 204
+          ctx.body = null
+          fs.writeFile(FILE_PATH, JSON.stringify(dataList), resolve)
+        } else {
+          ctx.body = { message: err.message }
+          ctx.status = 500
+          resolve()
+        }
       })
-    })
-    .get('/', (ctx, next) => {
-      fs.readFile(FILE_PATH, data => {
-        const dataList = JSON.parse(data) || []
-        ctx.body = dataList.sort((data1, data2) => data1.data > data2.data)
+    }))
+    .get('/', ctx => new Promise(resolve => {
+      fs.readFile(FILE_PATH, (err, data) => {
+        var dataList = []
+        if (!err) {
+          try {
+            dataList = JSON.parse(data)
+              .sort((data1, data2) => data1.data > data2.data)
+          } catch (err) {
+            //
+          }
+        }
+        ctx.body = dataList
         ctx.status = 200
-        next()
+        resolve()
       })
-    })
+    }))
     .routes()
 )
 
-module.exports = app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000)
